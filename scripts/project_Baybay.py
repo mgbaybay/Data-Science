6938d7198b903fc253ccb5514d69c7db4d1f68a8
MGB <87640602+mgbaybay@users.noreply.github.com>
2021-10-10 22:33:35 +0800

    Add files via upload

--- scripts/project_Baybay.py
+++ scripts/project_Baybay.py
@@ -1,43 +1,68 @@
+def fruit_list():
+  fruit = []
+  for i in range(len(fruits)):
+    fruit.append(fruits[i][0])
+  return fruit
+
+def update_stock():
+  print(fruit_list())
+  fruit_choice = str(input("Which of the fruits above would you like to update? ")).capitalize()
+  while fruit_choice not in fruit_list():
+    fruit_choice = str(input("Which of the fruits above would you like to update? ")).capitalize()
+  
+  for i in range(len(fruits)):
+    if (fruits[i][0].capitalize() == fruit_choice):
+      fruits[i][2] += int(input(f"How many stocks of {fruit_choice} came / left? "))
+      
+      if (fruits[i][2] < 0):
+        fruits[i][2] = 0 
+      print(f"Stocks of {fruit_choice} are now updated to {fruits[i][2]}") 
+
+def update_price():
+  fruit_choice = str(input("Which of the fruits above would you like to update? ")).capitalize()
+  while fruit_choice not in fruit_list():
+    fruit_choice = str(input("Which of the fruits above would you like to update? ")).capitalize()
+  
+  for i in range(len(fruits)):
+    if (fruits[i][0].capitalize() == fruit_choice):
+      fruits[i][1] = float(input(f"How much does {fruit_choice} now cost? "))
+      while (fruits[i][1] < 0):
+        fruits[i][1] = float(input(f"How much does {fruit_choice} now cost? "))
+      print(f"The price of {fruit_choice} is now Php {fruits[i][1]}") 
+
+def add_new_stock():
+  add_fruit = str(input("What is the fruit? ")).capitalize()
+  while add_fruit in fruit_list():
+    add_fruit = str(input("What is the fruit? ")).capitalize()
+
+  add_price = float(input("How much does it cost? "))
+  while (add_price < 0):
+    add_price = float(input("How much does it cost? "))
+  
+  add_stock = int(input("How many stocks are there? "))
+  while (add_stock < 0):
+    add_stock = int(input("How many stocks are there? "))
+  
+  new_stock = [add_fruit, add_price, add_stock]
+  fruits.append(new_stock)
+  print(f"{add_stock} items of {add_fruit} worth Php {add_price} is added.")
+
+def remove_stock():
+  fruit_choice = str(input("Which fruit do you want to delete? ")).capitalize()
+  while fruit_choice not in fruit_list():
+    fruit_choice = str(input("Which fruit do you want to delete? ")).capitalize()
+  
+  for i in range(len(fruits)):
+    if (fruits[i][0].capitalize() == fruit_choice):
+      fruit_index = i
+  del fruits[fruit_index]
+  print(f"Successfully deleted {fruit_choice}.")
+
 def total_assets():
-  output = "We have {} pieces of {} at {} each."
+  output = "We have {} pieces of {} at Php {} each."
   print(f"Good day, {name}. Here is the list of our stocks and prices:")
   for i in range(len(fruits)):
     print(output.format(fruits[i][2], fruits[i][0], fruits[i][1]))
-
-def update_stock():
-  fruit_choice = str(input("Which of the fruits above would you like to update? "))
-  new_stock = int(input(f"How many stocks of {fruit_choice} came / left? "))
-  for i in range(len(fruits)):
-    if (fruits[i][0] == fruit_choice):
-      fruits[i][2] = new_stock  
-  print(f"Stocks of {fruit_choice} are now updated to {new_stock}") 
-
-def update_price():
-  fruit_choice = str(input("Which of the fruits above would you like to update? "))
-  new_price = int(input(f"How much does {fruit_choice} now cost? "))
-  for i in range(len(fruits)):
-    if (fruits[0][i] == fruit_choice):
-      fruits[i][1] = new_price  
-  print(f"The price of {fruit_choice} is now {new_price}") 
-
-def add_new_stock():
-  add_fruit = str("What is the fruit? ")
-  add_price = float("How much does it cost? ")
-  add_stock = int("How many stocks are there? ")
-  new_stock = (add_fruit, add_price, add_stock)
-  fruits.append(new_stock)
-  print(f"{add_stock} items of {add_fruit} worth Php{add_price} is added.")
-
-def remove_stock():
-  remove_fruit = str(input("Which fruit do you want to delete? "))
-  for i in range(len(fruits)):
-    # match_fruit = fruits[i][0]
-    print(fruits[i][0])
-    print(fruits[i])
-    if (fruits[i][0] == remove_fruit):
-      del fruits[i]
-  print(f"Successfully deleted {remove_fruit}.")
-  total_assets()
 
 def choices():
   print("\n\t1. Update stock")
@@ -45,85 +70,32 @@ def choices():
   print("\t3. Add new stock")
   print("\t4. Remove existing stock")
   print("\t5. Total assets")
-  print("\t6. Exit")
-  num_choice = int(input("\nWhat would you like to do (Enter 1-6)? "))
-  return num_choice
-
+  print("\t6. Exit\n")
+  choice = int(input("What would you like to do (Enter 1-6)? ")) 
+  while (choice < 1 or choice > 6):
+    choice = int(input("What would you like to do (Enter 1-6)? "))
+  return choice
+      
 name = str(input("May I know your name? "))
-fruits = [("Apples", 35.0, 10), ("Banana", 25.0, 20), ("Cantaloupe", 15.0, 30)]
-
+fruits = [["Apple", 35.0, 10], ["Banana", 25.0, 20], ["Cantaloupe", 15.0, 30]]
 total_assets()
+
 choice = choices()
-if (choice != 6):
+while(choice != 6):
   if (choice == 1):
     update_stock()
+    choice = choices()
   elif (choice == 2):
     update_price()
+    choice = choices()
   elif (choice == 3):
     add_new_stock()
+    choice = choices()
   elif (choice == 4):
     remove_stock()
+    choice = choices()
   elif (choice == 5):
     total_assets()
+    choice = choices()
 
-else:
-  exit()
-# choice = {
-#   "1": update_stock(),
-#   "2": update_price(),
-#   "3": add_new_stock(),
-#   "4": remove_stock(),
-#   "5": total_assets(),
-#   "6": exit()
-# }
-
-# choice[choices()]()
-
-
-
-# output = "We have {} pieces of {} at {} each."
-
-# print(f"\nGood day, {name}. Here is the list of our stocks and prices:")
-# for i in range(len(fruits)):
-#   print(output.format(fruits[1][i], fruits[0][i], fruits[2][i]))
-# print("\n\t1. Update stock\t2. Update price\t3. Add new stock\t4. Remove existing stock\t5. Total assets\t6. Exit")
-
-# num_choice = int(input("\nWhat would you like to do (Enter 1-6)? "))
-
-# while (num_choice < 1 and num_choice > 6):
-#   num_choice = int(input("\nWhat would you like to do (Enter 1-6)? "))
-# else:
-#   print(f"{fruits[0]}") 
-#   fruit_choice = str(input("Which of the fruits above would you like to update? "))
-#   if (fruits[0][i].upper() == fruit_choice.upper()):
-
-
-# # def update_price():
-# #   fruit_price = [35.0, 25.0, 15.0]  
-# #   return
-
-# name = str(input("May I know your name? "))
-# # fruit_name = ["Apples", "Banana", "Cantaloupe"]
-# # fruit_stock = [10, 20, 30]
-# # fruit_price = [35.0, 25.0, 15.0]
-# # fruits = map(fruit_stock, fruit_name, fruit_price)
-# # fruits = [(10, "Apples", 35.0), (20, "Banana", 25.0), (30, "Cantaloupe", 15.0)]
-# fruits = [("Apples", "Banana", "Cantaloupe"), (10, 20, 30), (35.0, 25.0, 15.0)]
-# output = "We have {} pieces of {} at {} each."
-
-# print(f"\nGood day, {name}. Here is the list of our stocks and prices:")
-# for i in range(len(fruits)):
-#   # print(output.format(fruits[i][0], fruits[i][1], fruits[i][2]))
-#   print(output.format(fruits[1][i], fruits[0][i], fruits[2][i]))
-# print("\n\t1. Update stock")
-# print("\t2. Update price")
-
-# num_choice = int(input("\nWhat would you like to do (Enter 1-2)? "))
-
-# while (num_choice != 1 and num_choice != 2):
-#   num_choice = int(input("\nWhat would you like to do (Enter 1-2)? "))
-# else:
-#   print(f"{fruits[0]}")
-#   fruit_choice = str(input("Which of the fruits above would you like to update? "))
-#   new_price = int(input(f"How much does {fruit_choice} now cost? "))
-#   print(f"The price of {fruit_choice} is now {new_price}")
+  
